ce2a387805f72f532c1d0428d5983899f8f1358d
Armando <zavalaa4@msu.edu>
2025-07-02 14:59:15 -0400

    Search Page works
    Search page now displays videos based on the words searched.

--- src/app/search/[searchId]/page.js
+++ src/app/search/[searchId]/page.js
@@ -1,39 +1,37 @@
-import SearchBar from '../../components/SearchBar.js'
+import SearchBar from '../../../components/SearchBar'
 
-export default function Search() {
+export default async function Search({ params }) {
+    const { searchId } = params
+
+    const res = await fetch(
+        `http://localhost:3000/api/search?text=${encodeURIComponent(searchId)}&type=video`,
+        { cache: "no-store"}
+    );
+
+    const videos = await res.json()
 
     return (
     <div className="grid grid-rows-[20px_1fr_20px] gap-8">
         <div className='justify-items-center'>
             <SearchBar />
         </div>
-       <div>
-        <a className="flex flex-row gap-4" href="/video">
-            <img 
-            src="https://i.ytimg.com/vi/abc123/mqdefault.jpg" 
-            alt="Youtube Video"
-            height={160}
-            width={120}
-            />
-            <div>
-                <h2>Google’s new CLI tool hits different…</h2>
-                <p>Google has finally put Gemini in the command line, allowing you to hand total control of your machine over to AI. But how is their solution different from all the other similar tools out there?</p>
-            </div>
-        </a>
-       </div>
-       <div>
-        <a className="flex flex-row gap-4" href="/video">
-            <img 
-            src="https://i.ytimg.com/vi/abc123/mqdefault.jpg" 
-            alt="Youtube Video" 
-            height={160} 
-            width={120}/>
-            <div>
-                <h2>What Your IT Career Says About You</h2>
-                <p>Get a description of yourself based on what your IT Career is</p>
-            </div>
-        </a>
-       </div>
+        <div>
+        {videos.items?.map((vid) => {
+            return (<a key={vid} className="flex flex-row gap-4" href={`/video/${vid.id.videoId}`}>
+                <img 
+                src={vid.snippet.thumbnails.medium.url}
+                alt={vid.snippet.title}
+                height={160}
+                width={120}
+                />
+                <div>
+                    <h2>{vid.snippet.title}</h2>
+                    <p>{vid.snippet.description}</p>
+                </div>
+            </a>);
+        })
+        }
+        </div>
     </div>
     );
 };
